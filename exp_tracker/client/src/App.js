80f69c20bd3dd0dc328d534e2a5cfcd188a676a0
Anugrah070 <anugrah78123@gmail.com>
2023-12-25 23:51:43 +0530

    Starter

---
+++ exp_tracker/client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+
+      <div className='container mx-auto max-w-6xl'>
+      <h1 className='text-4xl'>EXPENSE TRACKER</h1>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
